94027842af414640f1eac765aed0cc94190588f3
Josh Cerri <joshcerrilight@gmail.com>
2019-04-17 12:29:34 -0500

    added rgbChaseExample sketch
    labelled version of an example sketch for rgbChase library

---
+++ rgbChase/rgbChaseExample/rgbChaseExample.ino
@@ -0,0 +1,14 @@
+#include <rgbChase.h>
+  //call class rgbChase and assgin a variable(pinA,pinB,pinC,delay);
+rgbChase myChase(3,5,6,4);
+void setup() {
+  //leave empty
+}
+
+void loop() {
+  // choose from desired effects
+//myChase.smooth();
+//myChase.step();
+//mychase.pulse();
+//mychase.redtowhite();
+}
